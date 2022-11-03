4e2635ae60b03866446a67c29a2c755ef5639f8b
Nguyễn Huy Hùng <y9susnhh@gmail.com>
2022-11-03 09:32:00 +0700

    Create 2022_11_03_022508_create_students_table.php

---
+++ database/migrations/2022_11_03_022508_create_students_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('students', static function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->boolean('gender');
+            $table->string('phone')->unique();
+            $table->string('address');
+            $table->string('identification');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+}
