ad8e4674985b68dfe674772e0b1dde0ea63a73ce
Nguyễn Huy Hùng <y9susnhh@gmail.com>
2022-11-04 09:01:46 +0700

    setup jwt

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    use ResponseTrait;
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    public function login(Request $request): JsonResponse
+    {
+        $credentials = $request->only('username', 'password');
+
+        if ($token = $this->guard()->attempt($credentials)) {
+            return $this->respondWithToken($token);
+        }
+
+        return $this->errorResponse('Unauthorized', 401);
+    }
+
+    /**
+     * @param $token
+     * @return JsonResponse
+     */
+    protected function respondWithToken($token): \Illuminate\Http\JsonResponse
+    {
+        return $this->successResponse([
+            'access_token' => $token,
+            'token_type'   => 'bearer',
+            'expires_in'   => $this->guard()->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function guard()
+    {
+        return Auth::guard();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,9 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::middleware('auth:api')->get('/users', function (Request $request) {
-//    return $request->users();
-//});
+Route::group([
+    'middleware' => 'api',
+    'prefix'     => 'auth',
+    'as'         => 'auth'
+], static function ($router) {
+
+    Route::post('login', [AuthController::class, 'login']);
+});
+
+
 Route::group(
     [
         'prefix' => 'admin',
@@ -28,8 +36,8 @@ Route::group(
             'prefix' => 'users',
             'as'     => 'users.'
         ], static function () {
+        Route::get('list', [UserController::class, 'list'])->name('list');
         Route::get('/{id?}', [UserController::class, 'profile'])->name('profile');
-        Route::get('list/{page?}', [UserController::class, 'list'])->name('list');
         Route::put('edit/{id?}', [UserController::class, 'update'])->name('update');
         Route::delete('/{id?}', [UserController::class, 'destroy'])->name('destroy');
     });
