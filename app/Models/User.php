69b266614150da01c23a574e661264ca53be682f
Nguyễn Huy Hùng <y9susnhh@gmail.com>
2022-11-07 08:37:07 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,9 +35,9 @@ class User extends Authenticatable implements JWTSubject
      *
      * @var array
      */
-//    protected $hidden = [
-//        'password', 'remember_token',
-//    ];
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 
     /**
      * The attributes that should be cast to native types.
